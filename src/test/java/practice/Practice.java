0fc5d88bfc9ad83c0bc0a4ac4263ccd2ebd7276b
Admin <Admin@RATHINASURYA>
2023-01-19 12:59:47 +0530

    Java Code

---
+++ src/test/java/practice/Practice.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class Practice {
+
+	public static void main(String[] args) {
+			{
+
+				int a[]= {12,24,10,50,62};
+				int res=a[0];
+				for(int i=0;i<4;i++)
+				{
+					if(res<a[i])
+					{
+						res=a[i];
+					}
+				}
+				System.out.println(res);
+	}
+
+}
