6f2654b471822773204abb2d486e45e0d4995f06
INDHU <INDHU@DESKTOP-7KBV175>
2023-01-19 12:05:55 +0530

    updated

---
+++ src/test/java/maven_scripts/Testscript4_Test.java
@@ -0,0 +1,18 @@
+package maven_scripts;
+
+import org.testng.annotations.Test;
+
+public class Testscript4_Test 
+{
+	@Test
+	public void sample7_Test()
+	{
+		System.out.println("Testscript7");
+	}
+	@Test
+	public void sample8_Test()
+	{
+		System.out.println("Testscript8");
+	}
+
+}
